5374715e6184e8c908eaeb7339a8e6eb4b57f2db
rsu-pindad <sautomated01@gmail.com>
2024-10-10 13:45:39 +0700

    .

--- app/Livewire/Sdm/RekapRespon.php
+++ app/Livewire/Sdm/RekapRespon.php
@@ -36,7 +36,7 @@ class RekapRespon extends Component
         // if (config('app.env') != 'local') {
         Pdf::view('pdf.dokumen-table', ['dataRekap' => $dataRekap])
             ->withBrowsershot(function (Browsershot $browsershot) {
-                // $browsershot
+                $browsershot
                 // ->setCustomTempPath('/tmp')
                 // ->setChromePath('/var/www/.cache/puppeteer/chrome/linux-131.0.6767.0/chrome-linux64/chrome')
                 // ->noSandbox()
@@ -50,36 +50,36 @@ class RekapRespon extends Component
                 // ->waitUntilNetworkIdle()
                 // ->emulateMedia('screen')
                 // ->showBackground();
-                // ->addChromiumArguments([
-                //     'allow-running-insecure-content',                                                  // https://source.chromium.org/search?q=lang:cpp+symbol:kAllowRunningInsecureContent&ss=chromium
-                //     'autoplay-policy' => 'user-gesture-required',                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kAutoplayPolicy&ss=chromium
-                //     'disable-component-update',                                                        // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableComponentUpdate&ss=chromium
-                //     'disable-domain-reliability',                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableDomainReliability&ss=chromium
-                //     'disable-features' => 'AudioServiceOutOfProcess,IsolateOrigins,site-per-process',  // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
-                //     'disable-print-preview',                                                           // https://source.chromium.org/search?q=lang:cpp+symbol:kDisablePrintPreview&ss=chromium
-                //     'disable-setuid-sandbox',                                                          // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSetuidSandbox&ss=chromium
-                //     'disable-site-isolation-trials',                                                   // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSiteIsolation&ss=chromium
-                //     'disable-speech-api',                                                              // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSpeechAPI&ss=chromium
-                //     'disable-web-security',                                                            // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableWebSecurity&ss=chromium
-                //     'disk-cache-size' => 33554432,                                                     // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
-                //     'enable-features' => 'SharedArrayBuffer',                                          // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
-                //     'hide-scrollbars',                                                                 // https://source.chromium.org/search?q=lang:cpp+symbol:kHideScrollbars&ss=chromium
-                //     'ignore-gpu-blocklist',                                                            // https://source.chromium.org/search?q=lang:cpp+symbol:kIgnoreGpuBlocklist&ss=chromium
-                //     'in-process-gpu',                                                                  // https://source.chromium.org/search?q=lang:cpp+symbol:kInProcessGPU&ss=chromium
-                //     'mute-audio',                                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kMuteAudio&ss=chromium
-                //     'no-default-browser-check',                                                        // https://source.chromium.org/search?q=lang:cpp+symbol:kNoDefaultBrowserCheck&ss=chromium
-                //     'no-pings',                                                                        // https://source.chromium.org/search?q=lang:cpp+symbol:kNoPings&ss=chromium
-                //     'no-sandbox',                                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kNoSandbox&ss=chromium
-                //     'no-zygote',                                                                       // https://source.chromium.org/search?q=lang:cpp+symbol:kNoZygote&ss=chromium
-                //     'use-gl'      => 'swiftshader',                                                    // https://source.chromium.org/search?q=lang:cpp+symbol:kUseGl&ss=chromium
-                //     'window-size' => '1920,1080',                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kWindowSize&ss=chromium
-                //     'single-process',                                                                  // https://source.chromium.org/search?q=lang:cpp+symbol:kSingleProcess&ss=chromium
-                // ]);
-                $browsershot
-                    ->setOption('args', ['--no-sandbox', '--disable-setuid-sandbox'])
+                ->addChromiumArguments([
+                    'allow-running-insecure-content',                                                  // https://source.chromium.org/search?q=lang:cpp+symbol:kAllowRunningInsecureContent&ss=chromium
+                    'autoplay-policy' => 'user-gesture-required',                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kAutoplayPolicy&ss=chromium
+                    'disable-component-update',                                                        // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableComponentUpdate&ss=chromium
+                    'disable-domain-reliability',                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableDomainReliability&ss=chromium
+                    'disable-features' => 'AudioServiceOutOfProcess,IsolateOrigins,site-per-process',  // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+                    'disable-print-preview',                                                           // https://source.chromium.org/search?q=lang:cpp+symbol:kDisablePrintPreview&ss=chromium
+                    'disable-setuid-sandbox',                                                          // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSetuidSandbox&ss=chromium
+                    'disable-site-isolation-trials',                                                   // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSiteIsolation&ss=chromium
+                    'disable-speech-api',                                                              // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSpeechAPI&ss=chromium
+                    'disable-web-security',                                                            // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableWebSecurity&ss=chromium
+                    'disk-cache-size' => 33554432,                                                     // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
+                    'enable-features' => 'SharedArrayBuffer',                                          // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+                    'hide-scrollbars',                                                                 // https://source.chromium.org/search?q=lang:cpp+symbol:kHideScrollbars&ss=chromium
+                    'ignore-gpu-blocklist',                                                            // https://source.chromium.org/search?q=lang:cpp+symbol:kIgnoreGpuBlocklist&ss=chromium
+                    'in-process-gpu',                                                                  // https://source.chromium.org/search?q=lang:cpp+symbol:kInProcessGPU&ss=chromium
+                    'mute-audio',                                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kMuteAudio&ss=chromium
+                    'no-default-browser-check',                                                        // https://source.chromium.org/search?q=lang:cpp+symbol:kNoDefaultBrowserCheck&ss=chromium
+                    'no-pings',                                                                        // https://source.chromium.org/search?q=lang:cpp+symbol:kNoPings&ss=chromium
+                    'no-sandbox',                                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kNoSandbox&ss=chromium
+                    'no-zygote',                                                                       // https://source.chromium.org/search?q=lang:cpp+symbol:kNoZygote&ss=chromium
+                    'use-gl'      => 'swiftshader',                                                    // https://source.chromium.org/search?q=lang:cpp+symbol:kUseGl&ss=chromium
+                    'window-size' => '1920,1080',                                                      // https://source.chromium.org/search?q=lang:cpp+symbol:kWindowSize&ss=chromium
+                    'single-process',                                                                  // https://source.chromium.org/search?q=lang:cpp+symbol:kSingleProcess&ss=chromium
+                ]);
+                // $browsershot
+                    // ->setOption('args', ['--no-sandbox', '--disable-setuid-sandbox'])
                     // ->setChromePath('/usr/bin/chromium-browser')
-                    ->setChromePath('/var/www/.cache/puppeteer/chrome/linux-129.0.6668.100/chrome-linux64/chrome')
-                    ->setNodeModulePath('/var/www/penilaian.pmu.my.id/node_modules/');
+                    // ->setChromePath('/var/www/.cache/puppeteer/chrome/linux-129.0.6668.100/chrome-linux64/chrome')
+                    // ->setNodeModulePath('/var/www/penilaian.pmu.my.id/node_modules/');
                     // ->timeout(60000)
                     // ->noSandbox()
                     // ->newHeadless();
